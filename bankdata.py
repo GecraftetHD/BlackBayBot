3d34adbe6fd7e06d03d845041cb00d6fac729b1a
GecraftetHD <77395698+GecraftetHD@users.noreply.github.com>
2021-02-23 20:33:31 +0100

    added mongo_db and added basic wallet creation

---
+++ bankdata.py
@@ -0,0 +1,21 @@
+import pymongo
+databases = pymongo.MongoClient("mongodb://localhost:27017/")
+
+db = databases["BlackBay"]
+
+clients = db["clients"]
+#{"user_id": INT, user_name": STR}
+
+wallets = db["wallets"]
+#{"amount": FLOAT, "users": LIST[object_id], channel_id": FLOAT
+
+print("Datenbank initialisiert.")
+
+
+
+
+
+
+#clients.insert_one({"user_id": "hallo", "amount": 123})
+#clients.insert_one({"user_id": "adawdaw", "amount": 312313})
+#print(clients.find_one({"user_id": "hallo"})["amount"])
--- main.py
+++ main.py
@@ -3,21 +3,33 @@ from discord.ext import commands
 import asyncio
 from dotenv import load_dotenv
 import os
-from cryptic_sdk import *
+import cryptic_sdk as cryptic
+import bankdata as db
+
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
-print(USERNAME, PASSWORD)
+cryptic_user = os.getenv('env_user')
+cryptic_password = os.getenv('env_password')
+cryptic_wallet = os.getenv('env_wallet_uuid')
+cryptic_key = os.getenv('env_wallet_key')
+print("--------------------------------------")
+print("logged in at Cryptic with Credentials:")
+#print("Username:", cryptic_user)
+#print("Password:", cryptic_password)
+print("--------------------------------------")
 
 bot = commands.Bot(command_prefix='<', intents=discord.Intents.all())
 bot.remove_command('help')
 
+client = cryptic.Client('wss://ws.cryptic-game.net')
+client.login(cryptic_user, cryptic_password)
+
 
 @bot.event
 async def on_ready():
-    print("Ich habe mich eingeloggt als User {}".format(bot.user.name))
+    print("Logged in as Discorduser: {}".format(bot.user.name))
     bot.loop.create_task(status_task())
 
 
@@ -36,12 +48,24 @@ async def status_task():
 
 @bot.command()
 async def get_money(ctx):
-    client = Client('wss://ws.cryptic-game.net')
-    print(USERNAME, PASSWORD)
-    client.login(USERNAME, PASSWORD)
-    print(client.getUser().name)
+
+    wallet: cryptic.Wallet = client.getUser().getWallet(cryptic_wallet, cryptic_key)
+    await ctx.send(wallet.amount)
+
+
+
+@bot.command()
+async def create_wallet(ctx: commands.Context):
+    user_id = ctx.author.id
+
+    category = discord.utils.get(ctx.guild.categories, name="tickets")
+    ticketNumber = len(db.wallets.find({}))
+    channel = ctx.channel if ctx.channel.name.startswith("ticket-") else await ctx.guild.create_text_channel(f'Ticket-{ticketNumber}', category=category).id
+
+
 
 
 bot.load_extension('cogs.help')
 bot.load_extension('cogs.bank')
 bot.run(TOKEN)
+client.logout()
